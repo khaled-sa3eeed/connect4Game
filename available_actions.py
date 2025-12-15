eddc764f4fd6b2f6cfc39054ec00d30be86efedf
Khaled Saeed <149275881+khaled-sa3eeed@users.noreply.github.com>
2025-12-16 01:00:37 +0200

    Delete available_actions.py

--- available_actions.py
+++
@@ -1,8 +0,0 @@
-
-def available_actions(self, current_state):
-        actions = []
-        for col in range(7):
-                if current_state[0][col] == " ":
-                    actions.append(col)
-
-        return actions
